479e677d879efb2d6e6f8c842862bc93727d0734
DanielCosta30 <nielcosta24@gmail.com>
2022-11-06 19:55:56 -0300

    feat(script-js) finalizado efeito de loader circle

--- js/script.js
+++ js/script.js
@@ -1,28 +1,105 @@
-$( document ).ready(function() {
-
+$(document).ready(function () {
   // Progress bar
   var containerA = document.getElementById("circleA");
 
   var circleA = new ProgressBar.Circle(containerA, {
-
-    color: '#65DAF9',
+    color: "#65DAF9",
     strokeWidth: 8,
     duration: 1400,
-    from: { color: '#AAA'},
-    to: { color: '#65DAF9'},
+    from: { color: "#AAA" },
+    to: { color: "#65DAF9" },
 
     // Gerando a Animação
 
-    step: function(state, circle) {
-      circle.path.setAttribute('stroke', state.color);
+    step: function (state, circle) {
+      circle.path.setAttribute("stroke", state.color);
 
       var value = Math.round(circle.value() * 60);
       circle.setText(value);
+    },
+  });
 
-    }
+  // Progress bar
+  var containerB = document.getElementById("circleB");
+
+  var circleB = new ProgressBar.Circle(containerB, {
+    color: "#65DAF9",
+    strokeWidth: 8,
+    duration: 1600,
+    from: { color: "#AAA" },
+    to: { color: "#65DAF9" },
+
+    // Gerando a Animação
+
+    step: function (state, circle) {
+      circle.path.setAttribute("stroke", state.color);
+
+      var value = Math.round(circle.value() *254);
+      circle.setText(value);
+    },
+  });
+
+  // Progress bar
+  var containerC = document.getElementById("circleC");
+
+  var circleC = new ProgressBar.Circle(containerC, {
+    color: "#65DAF9",
+    strokeWidth: 8,
+    duration: 2000,
+    from: { color: "#AAA" },
+    to: { color: "#65DAF9" },
+
+    // Gerando a Animação
+
+    step: function (state, circle) {
+      circle.path.setAttribute("stroke", state.color);
+
+      var value = Math.round(circle.value() * 32);
+      circle.setText(value);
+    },
 
   });
 
-  circleA.animate(1.0);
+    // Progress bar
+    var containerD = document.getElementById("circleD");
+
+    var circleD = new ProgressBar.Circle(containerD, {
+      color: "#65DAF9",
+      strokeWidth: 8,
+      duration: 2200,
+      from: { color: "#AAA" },
+      to: { color: "#65DAF9" },
+  
+      // Gerando a Animação
+  
+      step: function (state, circle) {
+        circle.path.setAttribute("stroke", state.color);
+  
+        var value = Math.round(circle.value() * 5245);
+        circle.setText(value);
+      },
+    });
+
+    // iniciando o loader quando o usuário chega na sessão dos dados
+
+    var dataAreaOffset = $('#data-area').offset();
+    var stop = 0;
+
+    $(window).scroll(function(e) {
+
+    var scroll = $(window).scrollTop();
+
+    if (scroll > (dataAreaOffset.top - 500) && stop == 0){
+
+    circleA.animate(1.0);
+    circleB.animate(1.0);
+    circleC.animate(1.0);
+    circleD.animate(1.0);
+
+    stop = 1;
+
+    }
+
+    });
 
 });
