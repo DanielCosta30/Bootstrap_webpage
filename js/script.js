d4d7b844dfc1671c6c78062765e195b35a0d8395
DanielCosta30 <nielcosta24@gmail.com>
2022-11-06 18:08:00 -0300

    feat(script-js) add Progress Bar/Objeto/CirclaA

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
 /* Primeiras instruções do Jquery */
 $(document).ready(function() {
 
-    
+    // Progress bar 
+    var containerA = document.getElementById("circleA");
+
+    var cicleA = new ProgressBar.cicle(containerA, {
+        color: '#64DAF9',
+        stroke: 8,
+        duration: 1400,
+        from: { color: '#AAA'},
+        to: { color: '#65DAF9'},
+
+        
+
+    });
+
 
 });
